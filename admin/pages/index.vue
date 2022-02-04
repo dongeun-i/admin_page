9e7609dde34a0556d737eed605e4539a2597daa8
DongEunLEE <dongeun1014@naver.com>
2022-02-04 17:58:39 +0900

    nuxt login 구현 진행중

--- admin/pages/index.vue
+++ admin/pages/index.vue
@@ -2,22 +2,44 @@
     <v-app>
          <v-card light elevation-0 class="col-12 mw-100 mh-100 vh-100 d-flex align-center justify-center flex-column">
             <v-form class="w-50 d-flex mb-12 flex-column">
-                <v-text-field 
+                <v-text-field
+                    outlined 
                     placeholder="ID"
+                    v-model="id"
                     type="text"
                 ></v-text-field>
                 <v-text-field
+                    outlined
+                    v-model="password"
                     placeholder="PASSWORD"
                     type="password"
                 ></v-text-field>
             </v-form>
-            <v-btn class="w-50">로그인</v-btn>
+            <v-btn class="w-50" @click="login">로그인</v-btn>
         </v-card>
     </v-app>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+    name: 'IndexPage',
+        data(){
+            return{
+                id:null,
+                password:null,
+            }
+        },
+    methods:{
+        login(){
+            let id = this.id
+            let password = this.password
+            if(id == 'admin' && password == 123123){
+                alert('로그인 성공 !');
+                this.$router.push('/main');
+            }else{
+                alert('아이디 또는 비밀번호를 확인해주세요.');
+            }
+        }
+    }
 }
 </script>
