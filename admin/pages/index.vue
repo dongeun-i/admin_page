042265798073330939b1348c47a7939f347ec0eb
DongEunLEE <dongeun1014@naver.com>
2022-02-04 17:22:56 +0900

    nuxt front 작업

---
+++ admin/pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-app>
+         <v-card light elevation-0 class="col-12 mw-100 mh-100 vh-100 d-flex align-center justify-center flex-column">
+            <v-form class="w-50 d-flex mb-12 flex-column">
+                <v-text-field 
+                    placeholder="ID"
+                    type="text"
+                ></v-text-field>
+                <v-text-field
+                    placeholder="PASSWORD"
+                    type="password"
+                ></v-text-field>
+            </v-form>
+            <v-btn class="w-50">로그인</v-btn>
+        </v-card>
+    </v-app>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
