b73fdb2626de2787b3a6e2f5265ecbc4d6782197
DongEunLEE <dongeun1014@naver.com>
2022-02-09 18:34:16 +0900

    데이터베이스 연결

--- admin/api/index.js
+++ admin/api/index.js
@@ -12,21 +12,46 @@ router.use((req, res, next) => {
   next();
 });
 
-router.post('/login', (req, res) => {
-  if (req.body.id === 'admin' && req.body.pw === 'a00000') {
-    req.session.authUser = req.body.id
-    return res.json({ id: 'admin' });
-  }
-  return res.json({ id: '' });
-});
+// router.post('/login', (req, res) => {
+//   if (req.body.id === 'admin' && req.body.pw === 'a00000') {
+//     req.session.authUser = req.body.id
+//     return res.json({ id: 'admin' });
+//   }
+//   return res.json({ id: '' });
+// });
 
-router.post('/logout', (req, res) => {
-  delete req.session.authUser;
-  res.json({ ok: true });
-});
+// router.post('/logout', (req, res) => {
+//   delete req.session.authUser;
+//   res.json({ ok: true });
+// });
 
 // Export the server middleware
 export default {
   path: '/apis',
   handler: router
 };
+
+
+// 데이터베이스 연결 
+
+var mysql = require('mysql');
+// Connection 객체 생성 
+var connection = mysql.createConnection({
+  host: '127.0.0.1',
+  port: 3306,
+  user: 'root',   
+  password: '!dlehddms1014',
+  database: 'admin'  
+});  
+// Connect
+connection.connect(function (err) {   
+  if (err) {     
+    console.error('mysql connection error');     
+    console.error(err);     
+    throw err;   
+  }else{
+    console.log('DB connection fin..')
+  }
+});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
---
+++ admin/api/login/login.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const login = require('./routes/login');
+
+app.get('/', function (req, res) {
+    
+    
+});
