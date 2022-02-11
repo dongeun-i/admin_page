a9cb33eaff66df086f1940a7c7093f2dffc7c329
DongEunLEE <dongeun1014@naver.com>
2022-02-11 16:52:16 +0900

    db 연결 테스트

--- admin/api/index.js
+++ admin/api/index.js
@@ -2,8 +2,13 @@ import express from 'express'
 
 // Create express router
 const router = express.Router()
+const app = express();
 
-const app = express()
+// db 모듈 연결
+var mysqlDB = require('./db');
+
+
+// router 연결준비
 router.use((req, res, next) => {
   Object.setPrototypeOf(req, app.request);
   Object.setPrototypeOf(res, app.response);
@@ -12,46 +17,21 @@ router.use((req, res, next) => {
   next();
 });
 
-// router.post('/login', (req, res) => {
-//   if (req.body.id === 'admin' && req.body.pw === 'a00000') {
-//     req.session.authUser = req.body.id
-//     return res.json({ id: 'admin' });
-//   }
-//   return res.json({ id: '' });
-// });
+app.get('/',(req,res,next)=>{
+  res.send('root');
+})
 
-// router.post('/logout', (req, res) => {
-//   delete req.session.authUser;
-//   res.json({ ok: true });
-// });
-
-// Export the server middleware
 export default {
-  path: '/apis',
+  path: '/api',
   handler: router
 };
 
-
-// 데이터베이스 연결 
-
-var mysql = require('mysql');
-// Connection 객체 생성 
-var connection = mysql.createConnection({
-  host: '127.0.0.1',
-  port: 3306,
-  user: 'root',   
-  password: '!dlehddms1014',
-  database: 'admin'  
-});  
-// Connect
-connection.connect(function (err) {   
-  if (err) {     
-    console.error('mysql connection error');     
-    console.error(err);     
-    throw err;   
-  }else{
-    console.log('DB connection fin..')
-  }
-});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+
+//CORS 해결
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
