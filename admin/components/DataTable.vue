d2329db153378695b87e9e87a2199e79287d5089
DongEunLEE <dongeun1014@naver.com>
2022-03-29 17:17:40 +0900

    filter 영역 작업 준비

--- admin/components/DataTable.vue
+++ admin/components/DataTable.vue
@@ -1,13 +1,19 @@
 <template>
 	<v-card class="elevation-0">
+		<template v-if="filter">
+			<v-sheet v-for="(filter,i) in filters" :key="i">
+				<v-checkbox v-if="filter.type == 'checkbox'">
+				</v-checkbox>
+			</v-sheet>
+		</template>
 		<v-card-title>
-		<v-text-field
-			v-model="search"
-			append-icon="mdi-magnify"
-			label="Search"
-			single-line
-			hide-details
-		></v-text-field>
+			<v-text-field
+				v-model="search"
+				append-icon="mdi-magnify"
+				label="Search"
+				single-line
+				hide-details
+			></v-text-field>
 		</v-card-title>
 		<v-data-table
 		:headers="tableHeader"
@@ -21,14 +27,12 @@ export default {
 	props:{
 		tableHeader:Array,
 		tableData:Array,
+		filters:Array
 	},
 	data(){
 		return{
 			search:''
 		}
 	},
-	mounted(){
-		console.log('tableHeader= ',this.tableHeader)
-	}
 }
 </script>
