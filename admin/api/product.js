a58563e2d0b0f2cbaed58f01bb66af836ae415f5
DongEunLEE <dongeun1014@naver.com>
2022-04-18 19:17:21 +0900

    ( GET ) 상품정보 카테고리정보 불러오기

--- admin/api/product.js
+++ admin/api/product.js
@@ -3,27 +3,19 @@ const { json } = require('express/lib/response');
 const router = Router();
 import callData from './db'
 
-function getProductList(userId) {
-	return new Promise(function(resolve, reject) {
-		let qs = `select P.* from product as P where P.userId = ${userId}`
-		let dataSet = callData(qs);
-		resolve(dataSet)
-	});
-}
-
-// 상품리스트
 router.get('/list',async function(req,res,next){
-	let userId = req.body.userId;
-	// 상품 가져오기
-	let resdata = {}
-	Promise.all([getProductList,])
+	let userId = req.header('userId');
+	let resdata = {};
 	{
-		let qs = `select P.* from product as P where P.userId = ${userId}`
+		// 상품리스트 가져오기
+		if(!userId) return
+		let qs = `select P.* ,C.label from product as P left outer join category as C on C.id = P.categoryId where P.userId = ${userId}`
 		let dataSet = await callData(qs);
 		resdata.productListset = dataSet;
 	}
-	// 필터영역부
+	
 	{
+		// 카테고리 정보 가져오기
 		let qs = 'select C.* from category as C ';
 		let CategotydataSet = await callData(qs);
 		resdata.categoryListSet = CategotydataSet;
@@ -35,7 +27,7 @@ router.get('/list',async function(req,res,next){
 router.get('/:id',async function(req,res,next){
 	console.log(req.params.id);
 	let productId = req.params.id;
-	let qs = `select P.* from product as P where P.id = ${productId}`
+	let qs = `select P.*, C.id from product as P left outer join category as C on C.id = P.categoryId where P.id = ${productId}`
 	let dataSet = await callData(qs)
 	res.send(dataSet);
 })
