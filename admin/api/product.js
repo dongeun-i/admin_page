a75e2c41077d71cb58ba7d5cedeaab679c1aeb61
dongeun-i <dongeun1014@naver.com>
2022-04-18 17:42:18 +0900

    상품리스트 카테고리 추가 준비

--- admin/api/product.js
+++ admin/api/product.js
@@ -3,12 +3,32 @@ const { json } = require('express/lib/response');
 const router = Router();
 import callData from './db'
 
+function getProductList(userId) {
+	return new Promise(function(resolve, reject) {
+		let qs = `select P.* from product as P where P.userId = ${userId}`
+		let dataSet = callData(qs);
+		resolve(dataSet)
+	});
+}
+
 // 상품리스트
 router.get('/list',async function(req,res,next){
 	let userId = req.body.userId;
-	let qs = `select P.* from product as P where P.userId = ${userId}`
-	let dataSet = await callData(qs);
-	res.send(dataSet);
+	// 상품 가져오기
+	let resdata = {}
+	Promise.all([getProductList,])
+	{
+		let qs = `select P.* from product as P where P.userId = ${userId}`
+		let dataSet = await callData(qs);
+		resdata.productListset = dataSet;
+	}
+	// 필터영역부
+	{
+		let qs = 'select C.* from category as C ';
+		let CategotydataSet = await callData(qs);
+		resdata.categoryListSet = CategotydataSet;
+	}
+	res.send(resdata);
 })
 
 // 상품 상세
