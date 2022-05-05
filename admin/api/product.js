a23f7c0b22b913f0fd5b0862c5c04a0a26069e29
dongeun-i <dongeun1014@naver.com>
2022-05-05 11:43:44 +0900

    상품등록 해결

--- admin/api/product.js
+++ admin/api/product.js
@@ -4,7 +4,7 @@ const router = Router();
 
 const multer = require('multer');
 const fs = require('fs');
-
+import query from './db'
 // storage setting for file
 // storage default
 const storage = multer.diskStorage({
@@ -18,10 +18,26 @@ const storage = multer.diskStorage({
 })
 const upload = multer({ storage:storage }).fields([{name:'thumbnail'},{name:'detail'}])
 
-import { Console } from 'console';
-import query from './db'
-
-
+/**
+ * @author dongeun
+ * @param path {string} 파일 경로
+ * @param old {string} 임시이름이 붙여진 파일
+ * @param name {string} 새로운 이름의 파일이름
+ * @description DB에 상품등록 이후 fs모듈을 통해서 임시이름으로 된 파일을 바꿔주는 함수 (static/img/product에 있는 이미지)
+ * */ 
+const change_filename = function(path,old,name){
+	return new Promise((res,rej)=>{
+		let old_file=`${path}/${old}`;
+		let new_file=`${path}/${name}`;
+		fs.rename(old_file,new_file,(err)=>{
+			if(err){
+				rej(err);
+			}else{
+				res('File rename fin.');
+			}
+		})
+	})
+}
 router.get('/list',async function(req,res,next){
 	let userId = req.header('userId');
 	// 상품리스트 가져오기
@@ -44,15 +60,22 @@ router.post('/register',async function(req,res,next){
 			let table_column = Object.keys(productInfo).join(",");
 			let table_value = Object.values(productInfo).join("','");
 			let qs = `INSERT INTO product (${table_column}) VALUES ('${table_value}')`
-			let thumbnail = req.files['thumbnail'][0]
+			let thumbnail = req.files['thumbnail'][0];
 			let thumbnail_type = thumbnail.mimetype.replace(/image\//g,'.')
 			query(qs).then(result=>{
-				// 	// 들어갔으면 이후 로직 
-					let insertId = result.insertId;
-					let newFileName = `/img/product/product_${insertId}${thumbnail_type}`
-					let qs = `UPDATE product as P SET P.thumbnail='${newFileName}' WHERE P.id=${insertId}`
-					query(qs);
-					res.send('Okay man')
+				// 	// 들어갔으면 이후 로직
+					let productId = result.insertId;
+					let newFileName = `product_${productId}${thumbnail_type}`
+					console.log('thumbnail=',thumbnail);
+					fs.rename(`static/img/product/${thumbnail.filename}`,`static/img/product/${newFileName}`,async(err)=>{
+						if(err){
+							console.err(err);
+						}else{
+							let qs = `UPDATE product as P SET P.thumbnail='/img/product/${newFileName}' WHERE P.id=${productId}`
+							let response = await query(qs);
+							res.send(response);
+						}
+					})
 			});
 		}
 		
@@ -78,7 +101,6 @@ router.put('/:id',async function(req,res,next){
 		let value = arr[1];
 		return arr = `${column} = '${value}'`
 	})
-	console.log('qsBase=',qsBase);
 	let qs = `UPDATE product AS P SET ${qsBase.join(',')} WHERE P.id=${productId}`
 	let result = await query(qs)
 	res.send(result);
