36ccf0a2658b85c669935db1394ce8b960ac6dc1
dongeun-i <dongeun1014@naver.com>
2022-05-05 12:44:15 +0900

    메인화면 레이아웃 구성해보기

--- admin/pages/main.vue
+++ admin/pages/main.vue
@@ -14,34 +14,65 @@ export default({
 		return{
 			sections:[{
 				layout:1,
-				width:50,
-				title:'주문/배송',
+				width:30,
+				title:'상품',
+				counts:[{
+					title:'판매중 상품',
+					count:3,
+				},{
+					title:'품절 상품',
+					count:5
+				},{
+					title:'비공개 상품',
+					count:1
+				},{
+					title:'판매중지 상품',
+					count:2
+				}],
+			},{
+				layout:null,
+				width:70,
+				title:null,
+				text:'상품들'
+			},{
+				layout:1,
+				width:30,
+				title:'주문',
 				counts:[{
 					title:'결제대기',
 					count:3,
 				},{
-					title:'신규주문',
+					title:'배송 준비',
 					count:5
 				},{
-					title:'배송준비',
+					title:'배송중',
 					count:1
 				},{
-					title:'배송중',
+					title:'배송완료',
 					count:2
+				}],
+			},{
+				layout:1,
+				width:30,
+				title:'교환 및 반품',
+				counts:[{
+					title:'교환 요청',
+					count:3,
+				},{
+					title:'반품 요청',
+					count:5
+				},{
+					title:'교환 ',
+					count:1
 				},{
 					title:'배송완료',
-					count:3
+					count:2
 				}],
 			},{
 				layout:null,
-				width:50,
-				title:'text',
-				text:'가나다라마바사'
-			},{
-				layout:null,
-				width:50,
-				title:'text',
-				text:'가나다라마바사'
+				width:40,
+				title:'진행중인 이벤트',
+				text:'배너들'
 			}],
 			// waitingForPaymentCount:3,// 결제 대기 수 
 			// newOrderCount:5, // 신규 주문 수
