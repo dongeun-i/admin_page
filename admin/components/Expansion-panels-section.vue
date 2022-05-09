ab5d89e60a65f010c985f4a00c1a33581b74a018
dongeun-i <dongeun1014@naver.com>
2022-05-09 20:54:49 +0900

    expansion-panels-section 완성

--- admin/components/Expansion-panels-section.vue
+++ admin/components/Expansion-panels-section.vue
@@ -1,15 +1,53 @@
 <template>
-	<v-expansion-panels>
+	<v-expansion-panels
+		v-model="expansionPanel"
+		multiple 
+	>
 		<v-expansion-panel v-for="(section,i) in sections" :key="i">
-			<v-expansion-panel-header>
+			<v-expansion-panel-header class="border-bottom">
 				{{section.title}}
 			</v-expansion-panel-header>
 			<v-expansion-panel-content>
-				<v-sheet v-for="(item,j) in section.children" :key="j">
+				<v-sheet v-for="(item,j) in section.children" :key="j" class="d-flex align-center">
 					<template v-if="item.layout == 'text'">
-						
+						<label class="col-2">{{item.label}}</label>
+						<p class="col-3 mb-0">{{item.text}}</p>
+					</template>
+
+					<template v-else-if="item.layout == 'input'">
+						<label class="col-2">{{item.label}}</label>
+						<v-text-field
+						class="col-3"
+						v-model="item.model">
+						</v-text-field>
+					</template>
+
+					<template v-else-if="item.layout == 'select'">
+						<label class="col-2">{{item.label}}</label>
+						<v-select
+							:items="item.values"
+							item-text="label"
+							item-value= "id"
+							v-model="item.model"
+							hide-details
+							class="col-3"
+							>
+						</v-select>
+					</template>
+
+					<template v-else-if="item.layout == 'textarea'">
+						<label class="col-2">{{item.label}}</label>
+						<v-textarea
+						outlined
+						v-model="item.model"
+						name="input-7-4"
+						>
+						</v-textarea>
 					</template>
 				</v-sheet>
+
+				<!-- 다른 추가하고 싶은것이 있을 경우 slot 활용 -->
+				<slot></slot>
 			</v-expansion-panel-content>
 		</v-expansion-panel>
 	</v-expansion-panels>
@@ -19,5 +57,11 @@ export default {
 	props:{
 		sections:Array
 	},
+	data(){
+		return{
+			showPassword:false,
+			expansionPanel:[0]
+		}
+	}
 }
 </script>
--- admin/pages/user/info.vue
+++ admin/pages/user/info.vue
@@ -1,19 +1,59 @@
 <template>
-	<v-card class="col-12 elevation-0">
-		<v-sheet class="d-flex justify-space-between align-center">
-			<v-card-title>판매자 기본정보</v-card-title>
-			<v-btn outlined @click="modifyProductInfo">수정하기</v-btn>
+	<v-card class="bg-gray col-12 elevation-0" height="100%">
+		<v-sheet color="#ccc" class="d-flex justify-space-between align-center">
+			<v-card-title class="font-weight-bold">판매자 기본정보</v-card-title>
+			<v-btn @click="modifyProductInfo">수정하기</v-btn>
 		</v-sheet>
-		<ExpansionPanels :panels="panels"/>
+		<ExpansionSection class="mb-5" :sections="section1">
+			<template>
+				<v-sheet class="d-flex align-center">
+					<label class="col-2">기존 비밀번호</label>
+					<v-text-field
+							v-model="oldPassword"
+							:append-icon="oldPasswordShow ? 'mdi-eye' : 'mdi-eye-off'"
+							:type="oldPasswordShow ? 'text' : 'password'"
+							class="col-3"
+							hide-details
+							@click:append="oldPasswordShow = !oldPasswordShow.show"
+						></v-text-field>
+				</v-sheet>
+				<v-sheet class="d-flex align-center">
+					<label class="col-2">새 비밀번호</label>
+					<v-text-field
+							v-model="newPassword"
+							:append-icon="newPasswordShow ? 'mdi-eye' : 'mdi-eye-off'"
+							:type="newPasswordShow ? 'text' : 'password'"
+							class="col-3"
+							hide-details
+							@click:append="newPasswordShow = !newPasswordShow.show"
+						></v-text-field>
+				</v-sheet>
+				<v-sheet class="d-flex align-center">
+					<label class="col-2">비밀번호 확인</label>
+					<v-text-field
+						v-model="verifyPassword"
+						:append-icon="verifyPasswordShow ? 'mdi-eye' : 'mdi-eye-off'"
+						:type="verifyPasswordShow ? 'text' : 'password'"
+						class="col-3"
+						@click:append="verifyPasswordShow = !verifyPasswordShow.show"
+						:rules="[passwordRules]"
+					></v-text-field>
+				</v-sheet>
+				
+			</template>		
+		</ExpansionSection>
+		<ExpansionSection :sections="section2">	
+		</ExpansionSection>
 	</v-card>
 </template>
 <script>
-import ExpansionPanels from '@/components/Expansion-panels-form'
+import ExpansionSection from '@/components/Expansion-panels-section'
 import Payload from '@/mixins/Payload.js'
 
 export default {
 	layout:'layout',
 	mixins:[Payload],
+	components:{ExpansionSection},
 	async asyncData({$axios}){
 		let userId = JSON.parse(sessionStorage.getItem('userInfo')).id;
 		if(userId){
@@ -26,14 +66,37 @@ export default {
 	data(){
 		return{
 			section1:[{
-				title:'아이디',
+				title:'로그인 정보',
 				model:null,
-				layout:'input',
+				children:[{
+					layout:'text',
+					label:'아이디',
+					text:'admin'
+				}],
 				target:'loginId'
-			},{
-				
-			}]
+			}],
+			oldPassword:null,
+			newPassword:null,
+			verifyPassword:null,
+			oldPasswordShow:false,
+			newPasswordShow:false,
+			verifyPasswordShow:false,
+			passwordRules:v=>{
+				if(this.newPassword != v){
+					return "비밀번호가 일치하지 않습니다"
+				}
+			}
+		}
+	},
+	methods:{
+		modifyProductInfo(){
+			console.log('수정')
 		}
 	}
 }
 </script>
+<style>
+.bg-gray{
+	background-color: #ccc ;
+}
+</style>
