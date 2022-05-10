62eb182acd314268addfdf48c9b60c48eea03bae
dongeun-i <dongeun1014@naver.com>
2022-05-10 22:25:00 +0900

    model => value || text 로 변경

--- admin/components/Expansion-panels-section.vue
+++ admin/components/Expansion-panels-section.vue
@@ -11,14 +11,14 @@
 				<v-sheet v-for="(item,j) in section.children" :key="j" class="d-flex align-center">
 					<template v-if="item.layout == 'text'">
 						<label class="col-2">{{item.label}}</label>
-						<p class="col-3 mb-0">{{item.text}}</p>
+						<p class="col-3 mb-0">{{item.value}}</p>
 					</template>
 
 					<template v-else-if="item.layout == 'input'">
 						<label class="col-2">{{item.label}}</label>
 						<v-text-field
 						class="col-3"
-						v-model="item.model">
+						v-model="item.value">
 						</v-text-field>
 					</template>
 
@@ -28,7 +28,7 @@
 							:items="item.values"
 							item-text="label"
 							item-value= "id"
-							v-model="item.model"
+							v-model="item.value"
 							hide-details
 							class="col-3"
 							>
@@ -39,15 +39,19 @@
 						<label class="col-2">{{item.label}}</label>
 						<v-textarea
 						outlined
-						v-model="item.model"
+						v-model="item.value"
 						name="input-7-4"
 						>
 						</v-textarea>
+					</template>
+					<template v-else-if="item.layout =='btn'">
+						<v-btn @click="item.onclick">{{item.text}}</v-btn>
 					</template>
 				</v-sheet>
 
 				<!-- 다른 추가하고 싶은것이 있을 경우 slot 활용 -->
 				<slot></slot>
+
 			</v-expansion-panel-content>
 		</v-expansion-panel>
 	</v-expansion-panels>
