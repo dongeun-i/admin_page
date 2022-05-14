96ce2615ba6e72803be065d2e6c66a0c34e82388
dongeun-i <dongeun1014@naver.com>
2022-05-14 16:24:15 +0900

    리스트 ,상태정보 불러오기

--- admin/api/order.js
+++ admin/api/order.js
@@ -4,11 +4,37 @@ const router = Router();
 import query from './db'
 
 router.get('/list', async function(req, res,next) {
-	let qs = 'select * from user'
+	let userId = req.header('userId');
+	let qs =`select ${[
+		'O.orderCode',
+		'O.orderDate as orderdate',
+		'O.recipientName',
+		'O.recipientTel',
+		'O.deliveryMemo',
+		'O.recipientJuso',
+		'O.recipientDetailJuso',
+		'O.salerId',
+		'OS.label as status',
+		'P.name as productName',
+		'P.price',
+		'P.discount',
+		'P.thumbnail',
+		'P.deliveryCost'
+	].join(",")} from ${[
+		'admin.order as O',
+		'left outer join product as P on P.id = O.productId',
+		'left outer join orderStatus as OS on OS.id = O.status',
+		'left outer join payType as PT on PT.id = O.payType'
+	].join(" ")} where O.salerId = ${userId}`
 	let dataSet = await query (qs);
 	res.send(dataSet)	
 });
 
+router.get('/status', async function(req,res,next){
+	let qs = 'select * from orderStatus';
+	let orderStatus = await query(qs);
+	res.send(orderStatus);
+})
 router.get('/:id', async function(req, res,next) {
 	let userId = req.params.id;
 	let qs = `select * from user as U where U.id = ${userId}`
