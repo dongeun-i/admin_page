747cebe8850b85e08025a1980535ba06867ccaac
dongeun-i <dongeun1014@naver.com>
2022-05-18 16:14:36 +0900

    정산 구조화 sample

--- admin/pages/calc/list.vue
+++ admin/pages/calc/list.vue
@@ -8,25 +8,30 @@
 		:items="items"
     >
 		<template v-slot:header>
-			<v-menu
+			<v-sheet class="d-flex align-center bg-gray" height="100px">
+				<v-menu
 				offset-y
 				:close-on-content-click="false"
 				max-width="300px"
 				min-width="auto"
 			>
-				<template v-slot:activator="{ on, attrs }">
-					<v-text-field
-					prepend-icon="mdi-calendar"
-					placeholder="날짜 선택"
-					v-bind="attrs"
-					v-on="on"
-					hide-details
-					background-color="#fff"
-					class="col-5"
-					></v-text-field>
-				</template>
-				<datepicker :minimumView="'month'" :maximumView="'month'" :inline="true" :language="languages[language]"></datepicker>
-			</v-menu>
+					<template v-slot:activator="{ on, attrs }">
+						<v-text-field
+						prepend-icon="mdi-calendar"
+						placeholder=" 날짜 선택"
+						v-bind="attrs"
+						v-on="on"
+						v-model="month"
+						hide-details
+						background-color="#fff"
+						class="col-5"
+						></v-text-field>
+					</template>
+					<datepicker v-model="month" :minimumView="'month'" :maximumView="'month'" :inline="true" :language="languages[language]"></datepicker>
+				</v-menu>
+				<v-btn>조회</v-btn>
+			</v-sheet>
+			
 		</template>
 		<template v-slot:default="props">
 			<v-row>
@@ -38,60 +43,28 @@
 				>
 					<v-card>
 					<v-card-title class="subheading font-weight-bold">
-						{{ item.name }}
+						{{month?new Date(month).getMonth()+1 +'월 정산대금':''}}
 					</v-card-title>
 
 					<v-divider></v-divider>
 
-					<v-list dense>
+					<v-list>
 						<v-list-item>
-						<v-list-item-content>Calories:</v-list-item-content>
-						<v-list-item-content class="align-end">
-							{{ item.calories }}
-						</v-list-item-content>
+							<v-list-item-content>총 매출액 : {{ item.sales }} 원 </v-list-item-content>
 						</v-list-item>
-
 						<v-list-item>
-						<v-list-item-content>Fat:</v-list-item-content>
-						<v-list-item-content class="align-end">
-							{{ item.fat }}
-						</v-list-item-content>
+							<v-list-item-content>총 상품 금액 : {{ item.sales }} 원</v-list-item-content>
 						</v-list-item>
-
 						<v-list-item>
-						<v-list-item-content>Carbs:</v-list-item-content>
-						<v-list-item-content class="align-end">
-							{{ item.carbs }}
-						</v-list-item-content>
+							<v-list-item-content>총 할인 금액 : {{ item.sales }} 원</v-list-item-content>
 						</v-list-item>
-
 						<v-list-item>
-						<v-list-item-content>Protein:</v-list-item-content>
-						<v-list-item-content class="align-end">
-							{{ item.protein }}
-						</v-list-item-content>
+							<v-list-item-content>배송비 합계 : {{ item.sales }} 원</v-list-item-content>
 						</v-list-item>
-
 						<v-list-item>
-						<v-list-item-content>Sodium:</v-list-item-content>
-						<v-list-item-content class="align-end">
-							{{ item.sodium }}
-						</v-list-item-content>
+							<v-list-item-content>수수료 합계 : {{ item.sales }} 원</v-list-item-content>
 						</v-list-item>
-
-						<v-list-item>
-						<v-list-item-content>Calcium:</v-list-item-content>
-						<v-list-item-content class="align-end">
-							{{ item.calcium }}
-						</v-list-item-content>
-						</v-list-item>
-
-						<v-list-item>
-						<v-list-item-content>Iron:</v-list-item-content>
-						<v-list-item-content class="align-end">
-							{{ item.iron }}
-						</v-list-item-content>
-						</v-list-item>
+						
 					</v-list>
 					</v-card>
 				</v-col>
@@ -108,15 +81,13 @@ export default {
 	data(){
 		return{
 			items: [{
-				name: null,
-				calories: null,
-				fat: null,
-				carbs: null,
-				protein: null,
-				sodium: null,
-				calcium: null,
-				iron: null,
+				sales : 0,
+				productPrice: 0,
+				deliveryPrice: 0,
+				charge: 0,
 			}],
+			month:null,
+			salesProductList:[],
 			language: "ko",
 			languages: lang,
 		}
@@ -126,3 +97,8 @@ export default {
 	},
 }
 </script>
+<style scoped>
+	.bg-gray{
+		background-color: #ccc ;
+	}
+</style>
